603b88176fae3220d727ec9fa34c3b2f1c813e64
Matt Whitesides <mbwxd4@mail.missouri.edu>
2014-12-31 13:18:54 -0600

    ?

---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="static/css/main.css">
+		<link rel="shortcut icon" href="/static/icons/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="/static/icons/favicon.ico" type="image/x-icon">
+	</head>	
+	<body>
+		<div class="TextBlock center">
+			<div class="orange"><h1>matt </h1></div>
+			<div class="white"><h1>WHITESIDES</h1></div>
+			<h3 class="white">Website currently under construction. If you have any questions feel free to Email me.</h3>
+			<h3 class="white">Contact Info: <a href="mailto:mbwxd4@mail.missouri.edu">mbwxd4@mail.missouri.edu</a></h3>
+			<h3 class="white">Or visit me on <a href="https://github.com/mattwhitesides" target="_blank">GitHub</a> and <a href="http://www.linkedin.com/pub/matthew-whitesides/a0/879/429/" target="_blank">LinkedIn</a></h3>
+			<h3 class="orange">Thanks, Matt</h3>
+		</div>	
+	</body>
+</html>
